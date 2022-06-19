7e8fcdf73cda1312bddb380e76e931ff9520cc39
Mirasan <72389804+MirelAlex@users.noreply.github.com>
2022-06-20 00:27:58 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function setup() {
 
 function draw() {
   background(0);
-  translate(windowWidth / 3, 200);
+  translate(windowWidth / 3 - 20, 200);
   fill(255);
   textSize(32);
   text(`N = ${slidernumber.value()}`, -30, -120);
@@ -22,25 +22,11 @@ function draw() {
   for (let i = 0; i < slidernumber.value(); i++) {
     let prevx = x;
     let prevy = y;
-
-    // let n = i* 2+1;  //square wave and trinagle wave
-    // // let n = i+1 ;
-    // numarator= pow((-1), (n^2-1)/2);
-    // let radius = (75*8/sq(PI)) * ( numarator/ sq(n ));
-    // if(n<20){
-    // // console.log(numarator);
-    // // console.log(n);
-    // }
-    // //i= Complex.I
-    // /*sawtooth */
-    // // if(n%2==0){
-    // //   radius = -radius;
-    // // }
-    // // //
+    
     let n = i * 2 + 1; //1,3,5,7...
     // let n = i + 1; //1,2,3,4...
 
-    let radius = 55 * (4 / (n * PI));
+    let radius = 45 * (4 / (n * PI));
     x += radius * cos(n * time);
     y += radius * sin(n * time);
 
@@ -48,16 +34,16 @@ function draw() {
     noFill();
     ellipse(prevx, prevy, radius * 2);
 
-    //fill(255);
+
     stroke(255);
     line(prevx, prevy, x, y);
-    //ellipse(x, y, 8);
+
   }
 
   wave.unshift(y);
 
-  translate(200, 0);
-  line(x - 200, y, 0, wave[0]);
+  translate(80, 0);
+  line(x - 80, y, 0, wave[0]);
   beginShape();
   noFill();
   for (let i = 0; i < wave.length; i++) {
